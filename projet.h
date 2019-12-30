34e6f71cc283f5a2f2c1000c44318859343579b0
nitsou63 <59048305+nitsou63@users.noreply.github.com>
2019-12-30 15:34:40 +0100

    Update projet.h

--- projet.h
+++ projet.h
@@ -1,4 +1,53 @@
 #include <stdio.h>
 
+//Different types de logements
+typedef enum{
+  chambre, studio, T1, T2
+}Typelog;
+
+//Type Booléen
+typedef enum{
+  false, true
+}Booleen;
+
+//Different types de civilité
+typedef enum{
+  Mr, Mme
+}Civilité;
+
+//Structure des logements
+typedef struct{
+  // Forme de idLogement : 'L00001','L00002'...
+  char idLogement[6];
+  char * nom;
+  Typelog type;
+  Booleen dispo;
+  Booleen handicap;
+  // Forme de idEtud : 'E00001','E00002'...
+  char idEtud[6];
+}Logement;
+
+//Structure des étudiants
+typedef struct{
+  // Forme de idEtud : 'E00001','E00002'...
+  char idEtud[6];
+  Civilité civilité;
+  char *nom;
+  char *prenom;
+  Booleen bourse;
+  int echelon;
+  Booleen handicap;
+}Etudiant;
+
+//Structure des Demandes en Attente
+typedef struct{
+  // Forme de idDemande : 'D00001','D00002'...
+  char idDemande[6];
+  // Forme de idEtud : 'E00001','E00002'...
+  char idEtud[6];
+  int echelon;
+  char *nomCite;
+  Typelog type;
+}DemandeA;
 
 void global(void);
